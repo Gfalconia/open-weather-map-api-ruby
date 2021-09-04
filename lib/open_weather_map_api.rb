fcfaed84ad8520a0ab13da89423268799e40a1ed
Ilya Kovalchuk <stormscreame@gmail.com>
2021-09-04 14:58:52 +0400

    first commit

---
+++ lib/open_weather_map_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "open_weather_map_api/version"
+
+module OpenWeatherMapApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/open_weather_map_api/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module OpenWeatherMapApi
+  VERSION = "0.1.0"
+end
